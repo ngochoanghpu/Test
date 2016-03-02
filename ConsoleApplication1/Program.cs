246c433305a4eb6f05ee00b0a2ff790ab2b6826d
hoangnn <hoangnn@dev.ndex.net>
2016-03-02 09:15:19 +0700

    Test
    Signed-off-by: hoangnn <hoangnn@dev.ndex.net>

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Console.Write("Hello GitHub 5");
+            Console.Write("Hello GitHub 6");
         }
     }
 }
