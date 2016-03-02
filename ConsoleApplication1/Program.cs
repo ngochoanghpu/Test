fb4e8e959a677a7b4af14d4b8c464c2d76470ea5
hoangnn <hoangnn@dev.ndex.net>
2016-03-02 08:50:17 +0700

    Hello Git
    Signed-off-by: hoangnn <hoangnn@dev.ndex.net>

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Console.Write("Hello World 2");
+            Console.Write("Hello GitHub");
         }
     }
 }
