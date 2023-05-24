dc281aafd3c6ce6e996917204e90ed53d48ca56c
ngochoanghpu <ngochoanghpu220186@gmail.com>
2023-05-24 16:43:23 +0700

    Add files via upload

--- docker.sh
+++ docker.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 sudo apt-get update -y
+sudo apt install curl -y
+sudo apt install unrar -y
 curl -fsSL https://get.docker.com -o get-docker.sh
 DRY_RUN=1 sh ./get-docker.sh
 sudo sh get-docker.sh
